8ec87bf28a5e54cc6fc8920a3b3156054240622e
parksuja <njoy4089@nate.com>
2021-11-01 19:24:47 +0900

    2021-11-01

---
+++ src/components/exercises/FirstExer.js
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import Responsive from "../common/Responsive";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { CardActionArea } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const Wrapper = styled(Responsive)`
+  vertical-align: middle;
+  // 가운데 수직 정렬함
+`;
+
+const ExerCard = styled.div`
+  display: inline-block;
+`;
+//여러개의 엘리먼트를 한줄에 배치
+
+const FirstExer = () => {
+  const useStyles = makeStyles({
+    // material에서 ui 스타일적용할 수 있도록
+    root: {
+      maxWidth: "350px",
+      height: "100px",
+      width: "170px",
+      display: "inline-block",
+    },
+    title: {
+      fontSize: 15,
+      marginBottom: "10px",
+    },
+    arrow: {
+      display: "inline-block",
+      marginBottom: "100px",
+      marginLeft: "30px",
+      marginRight: "30px",
+      color: "#1971c2",
+    },
+  });
+
+  const classes = useStyles();
+  return (
+    <>
+      <Wrapper>
+        <ExerCard>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                STEP 1
+              </Typography>
+              <Typography variant="h5" component="h2">
+                스쿼트
+              </Typography>
+            </CardContent>
+          </Card>
+          <ArrowForwardIosIcon className={classes.arrow} />
+        </ExerCard>
+        <ExerCard>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                STEP 2
+              </Typography>
+              <Typography variant="h5" component="h2">
+                런지
+              </Typography>
+            </CardContent>
+          </Card>
+          <ArrowForwardIosIcon className={classes.arrow} />
+        </ExerCard>
+        <ExerCard>
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                STEP 3
+              </Typography>
+              <Typography variant="h5" component="h2">
+                숄더프레스
+              </Typography>
+            </CardContent>
+          </Card>
+          <ArrowForwardIosIcon className={classes.arrow} />
+        </ExerCard>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              STEP 4
+            </Typography>
+            <Typography variant="h5" component="h2">
+              나무자세
+            </Typography>
+          </CardContent>
+        </Card>
+      </Wrapper>
+    </>
+  );
+};
+
+export default FirstExer;
--- src/container/exercise/ExerciseContainer.js
+++ src/container/exercise/ExerciseContainer.js
@@ -6,23 +6,69 @@ import palette from "../../lib/style/palette";
 import { useRef } from "react";
 // relative여기서 태그의 위치를 살짝 변경하고 싶을 때 position: relative를 사용합니다
 import "./ExerciseContainer.css";
+import First from "./First";
 
-function ExerciseContainer() {
+const EC = styled.div`
+position : relative;
+}
+`;
+const Spacer = styled.div`
+  height: 3.5rem;
+`;
+
+const StyledButton = styled(Button)`
+  background: ${palette.gray[7]};
+  &:hover {
+    background: ${palette.gray[5]};
+  }
+  position: absolute;
+  bottom: -3%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+let squatCount = 15;
+let lungeCount = 15;
+let shoulderCount = 15;
+const setSquatCount = (squat) => {
+  if (squat === "") squatCount = 15;
+  else if (squat < 0) squatCount = parseInt(-squat);
+  else squatCount = parseInt(squat);
+};
+const setLungeCount = (lunge) => {
+  if (lunge === "") lungeCount = 15;
+  else if (lunge < 0) lungeCount = parseInt(-lunge);
+  else lungeCount = parseInt(lunge);
+};
+const setShoulderCount = (shoulder) => {
+  if (shoulder === "") shoulderCount = 15;
+  else if (shoulder < 0) shoulderCount = parseInt(-shoulder);
+  else shoulderCount = parseInt(shoulder);
+};
+const ExerciseContainer = () => {
+  const [first, setFirst] = useState(true);
+  const [showResults, setShowResults] = useState(false);
+
+  const videoOn = () => {
+    setShowResults(true);
+    setFirst(false);
+  };
   return (
     <>
-      <div>
-        <video
-          id="video"
-          width="640"
-          height="480"
-          autoplay
-          muted
-          playsinline
-        ></video>
-        <canvas id="canvas"></canvas>
-      </div>
+      <EC>
+        {first && (
+          <div>
+            <Spacer />
+            <First
+              setSquatCount={setSquatCount}
+              setLungeCount={setLungeCount}
+              setShoulderCount={setShoulderCount}
+            />
+            <Spacer />
+            <StyledButton onClick={videoOn}>시작</StyledButton>
+          </div>
+        )}
+      </EC>
     </>
   );
-}
-
+};
 export default ExerciseContainer;
